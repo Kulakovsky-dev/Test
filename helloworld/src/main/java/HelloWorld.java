42effa22b092d80111af83869fc3b99c96531843
KulakovskySergey <59972461+KulakovskySergey@users.noreply.github.com>
2020-01-22 15:08:24 +0300

    Repeating Commit для оставшихся 4 файлов

--- helloworld/src/main/java/HelloWorld.java
+++ helloworld/src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class HelloWorld {
     public static void main(String[] args) {
         int[] arr = new int[] {1,2,3,4,5};
-        System.out.println("Hello World");
+        System.out.println("Hello World это домашнее задание тест !!!");
         System.out.println(Arrays.toString(arr));
     }
 }
